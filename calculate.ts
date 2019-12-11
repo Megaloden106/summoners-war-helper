bd3355e113ada05c2348611e6bfb9c735f913554
EddieLo <eddielo106@gmail.com>
2019-12-10 19:05:29 -0800

    tweak: Update to latest stat requirements

--- calculate.ts
+++ calculate.ts
@@ -1,12 +1,12 @@
-import { target, builds, getSynergy, format } from './util';
+import { target, builds, getSynergy, format, multiplier } from './util';
 import { Rune, Count, StatEffect, Build } from './global.types';
-import { RuneSet, RuneQuality, StatType, Action, RuneStat } from './global.enums';
+import { RuneSet, RuneQuality, StatType, Action, RuneStat, Monster } from './global.enums';
 
 export const calculate = (rune: Rune) => {
   const {
     rune_id,
     slot_no,
-    // rank,
+    rank,
     class: rune_grade,
     set_id,
     upgrade_curr,
@@ -14,6 +14,7 @@ export const calculate = (rune: Rune) => {
     prefix_eff,
     sec_eff, // [type, value, gem, grind]
     extra,
+    unit,
   } = rune;
   const count: Count = {
     current: 0,
@@ -21,8 +22,12 @@ export const calculate = (rune: Rune) => {
   }
   const synergy = getSynergy(rune);
   let keep = false;
+  const keepBuilds: string[] = [];
 
-  let description = `+${upgrade_curr} ${RuneQuality[extra]} ${rune_grade}* Slot ${slot_no} ${RuneSet[set_id]} Rune ID: ${rune_id}\n`;
+  // if (unit && !Monster[unit]) {
+  //   console.log(unit)
+  // }
+  let description = `+${upgrade_curr} ${RuneQuality[extra]} ${rune_grade}* Slot ${slot_no} ${RuneSet[set_id]} Rune ID: ${rune_id} Owner: ${unit ? Monster[unit] : 'INV'}\n`;
   description += format(StatType.Main, pri_eff, count);
   description += format(StatType.Prefix, prefix_eff, count);
   sec_eff.forEach((stat: StatEffect) => description += format(StatType.Sub, stat, count));
@@ -34,24 +39,28 @@ export const calculate = (rune: Rune) => {
         description += `${k}: ${count.current + synergy}/${count.total + synergy}/${target.total[k]} `;
         if (count.total + synergy >= target.total[k]) {
           keep = true;
+          keepBuilds.push('CV');
         }
         return;
       }
 
-      description += `${k}: ${(count[k] || 0) + synergy}/${target.total[k]} `;
-      if ((count[k] || 0) + synergy >= target.total[k]) {
+      let targetTotal = target.total[k];
+      if (slot_no % 2 === 0) {
+        // @ts-ignore
+        targetTotal -= multiplier(RuneStat[k], 3);
+      }
+      description += `${k}: ${count[k] || 0}/${targetTotal} `;
+      if ((count[k] || 0) >= targetTotal) {
         keep = true;
       }
     });
   description += '\n';
-
-  const keepBuilds: string[] = [];
   builds.forEach((build: Build) => {
     const sum = build.stats.reduce((acc, curr) => {
       return acc + (count[RuneStat[curr]] || 0)
     }, 0);
-    description += `${build.name}: ${sum + synergy}/${build.target} `
-    if (sum + synergy >= build.target) {
+    description += `${build.name}: ${sum}/${build.target} `
+    if (sum >= build.target) {
       keep = true;
       keepBuilds.push(build.name);
     }
@@ -60,7 +69,7 @@ export const calculate = (rune: Rune) => {
 
   // determine type of action
   const reapp = slot_no % 2 === 0 && !keep && extra === 5 && rune_grade === 6 && upgrade_curr >= 12;
-  const upgrade = upgrade_curr < 12 || (slot_no % 2 === 0 && upgrade_curr < 15);
+  const upgrade = upgrade_curr < 12;
   const action = reapp ? Action.Reapp
     : upgrade ? Action.Upgrade
       : keep ? Action.Keep : Action.Sell;
@@ -86,5 +95,6 @@ export const calculate = (rune: Rune) => {
     upgrade,
     reapp,
     total: count.total + synergy,
+    set: set_id,
   };
 };
--- search.ts
+++ search.ts
@@ -1,44 +1,76 @@
-import { readFile } from 'fs';
+import { readFile, writeFile, appendFile } from 'fs';
 import { promisify } from 'util';
 import { calculate } from './calculate';
 import { Filter } from './global.enums';
 import { SumWarData } from 'global.types';
+import { unitFilter } from './util';
 
 const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
+const appendFileAsync = promisify(appendFile);
 
-const execute = async (filter: Filter | null, isAll: boolean) => {
-  const data = await readFileAsync('mega1odon-11762846.json', 'utf8');
-  const { runes, unit_list }: SumWarData = JSON.parse(data);
+const execute = async (filter: Filter | null, isAll?: boolean, isLocked?: boolean, isEquiped?: boolean) => {
+  const data = await readFileAsync('/Users/eddielo/Desktop/Summoners War Exporter Files/mega1odon-11762846.json', 'utf8');
+  let { runes, unit_list }: SumWarData = JSON.parse(data);
 
-  if (isAll) unit_list.forEach(unit => runes.push(...unit.runes));
+  if (isEquiped) {
+    runes = [];
+  }
+
+  if (isAll) {
+    unit_list.forEach(unit => {
+      if (isLocked && unitFilter.includes(unit.unit_master_id)) {
+        return;
+      }
+
+      const unitRunes = unit.runes
+        .map(r => ({ ...r, unit: unit.unit_master_id }))
+      runes.push(...unitRunes)
+    });
+  }
   const runeDetails = runes
     .map(rune => calculate(rune))
-    .sort((a, b) => a.total - b.total);
+    // .filter(({ total }) => total < 62)
+    // .sort((a, b) => a.total - b.total) // ASC
+    // .sort((a, b) => b.total - a.total) // DESC
+    .sort((a, b) => a.set - b.set || a.slot - b.slot);
 
   runeDetails.forEach((stat) => {
     if (!stat.upgrade && !stat.keep && !stat.reapp && filter === Filter.Sell) {
-      console.log(stat.description);
+      appendFileAsync('runes.txt', stat.description);
     }
     if (stat.keep && filter === Filter.Keep) {
-      console.log(stat.description);
-    }
-    if (stat.upgrade && stat.keep && stat.slot % 2 === 0 && filter === Filter.Upgrade15) {
-      console.log(stat.description);
+      appendFileAsync('runes.txt', stat.description);
     }
     if (stat.upgrade && filter === Filter.Upgrade) {
-      console.log(stat.description);
+      appendFileAsync('runes.txt', stat.description);
+    }
+    if (stat.upgrade && stat.keep && stat.slot % 2 === 0 && filter === Filter.Upgrade15) {
+      appendFileAsync('runes.txt', stat.description);
     }
     if (stat.reapp && filter === Filter.Reapp) {
-      console.log(stat.description);
+      appendFileAsync('runes.txt', stat.description);
     }
   });
-  console.log('TOTAL:   ', runes.length);
-  // console.log('KEEP:    ', runeDetails.sort((a, b) => b.total - a.total)[0].description);
-  console.log('KEEP:    ', runeDetails.filter(({ keep }) => keep).length);
-  console.log('REAPP:   ', runeDetails.filter(({ reapp }) => reapp).length);
-  console.log('UPGRADE: ', runeDetails.filter(({ upgrade }) => upgrade).length);
-  console.log('UPGRADE15: ', runeDetails.filter(({ upgrade, keep, slot }) => upgrade && keep && slot % 2 === 0).length);
-  console.log('SELL:    ', runeDetails.filter(({ upgrade, keep, reapp }) => !upgrade && !keep && !reapp).length);
+  console.log('TOTAL:    ', runes.length);
+  console.log('KEEP:     ', runeDetails.filter(({ keep }) => keep).length);
+  // console.log('KEEP62:   ', runeDetails.filter(({ total }) => total >= 62).length);
+  // console.log('KEEP65:   ', runeDetails.filter(({ total }) => total >= 65).length);
+  // console.log('KEEP70:   ', runeDetails.filter(({ total }) => total >= 70).length);
+  // console.log('KEEP80    ', runeDetails.filter(({ total }) => total >= 80).length);
+  console.log('REAPP:    ', runeDetails.filter(({ reapp }) => reapp).length);
+  console.log('UPGRADE:  ', runeDetails.filter(({ upgrade }) => upgrade).length);
+  console.log('SELL:     ', runeDetails.filter(({ upgrade, keep, reapp }) => !upgrade && !keep && !reapp).length);
+  console.log('SLOTODD   ', runeDetails.filter(({ keep, slot }) => keep && slot % 2 !== 0).length);
+  console.log('SLOT1:    ', runeDetails.filter(({ keep, slot }) => keep && slot === 1).length);
+  console.log('SLOT3:    ', runeDetails.filter(({ keep, slot }) => keep && slot === 3).length);
+  console.log('SLOT5:    ', runeDetails.filter(({ keep, slot }) => keep && slot === 5).length);
+  console.log('SLOTEVEN: ', runeDetails.filter(({ keep, slot }) => keep && slot % 2 === 0).length);
+  console.log('SLOT2:    ', runeDetails.filter(({ keep, slot }) => keep && slot === 2).length);
+  console.log('SLOT4:    ', runeDetails.filter(({ keep, slot }) => keep && slot === 4).length);
+  console.log('SLOT6:    ', runeDetails.filter(({ keep, slot }) => keep && slot === 6).length);
 }
 
-execute(Filter.Keep, false);
+// filter, isAll, isLocked, isEquiped
+writeFileAsync('runes.txt', '')
+  .then(() => execute(Filter.Sell, false, false, false));
